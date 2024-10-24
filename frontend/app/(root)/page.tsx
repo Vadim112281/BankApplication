47c94af92eafa3ff86914a4bfe6f86ce1bb42d1a
Vadim <matokvadym@gmail.com>
2024-10-24 15:25:53 +0300

    Home Page
    A few updates

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,10 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 function Home() {
+  const loggenIn = { firstName: 'Vadim'};
+
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggenIn?.firstName || 'Guest'} 
+            subtext="Acces and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
